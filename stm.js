22d0d8ef55afdc2fd4465bf935a6d7bc5965c23e
David Caldwell <david@porkrind.org>
2014-01-29 00:11:13 -0800

    GPLv3 this bad boy.

--- stm.js
+++ stm.js
@@ -1,4 +1,19 @@
-//  Copyright (c) 2013 David Caldwell,  All Rights Reserved.
+// stm.js - ServeToMe compatible server to stream videos to StreamToMe
+// Copyright (c) 2013-2014 David Caldwell.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var app_port = 9969;
 var app_root = [ { path:'/home/david/Movies/', name:'Movies' } ];
 var types= ["aif","m2ts","ts","flac","wmv","ogm","ogg","wma","m4a","vob","dif","dv","flv","asf","mp2","mp3","ac3","aac","mpeg4","mp4","m4v","mpeg","mkv","mpg","mov","gvi","avi"];
